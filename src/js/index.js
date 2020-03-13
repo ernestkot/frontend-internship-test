c28ec17b68765d7bfbefc65d15f79eb9fbee212a
Ernest <ernest.kot98gmail.com>
2020-03-13 19:39:39 +0100

    Task 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
 /* Here goes your JS code */
+
+let divPopup = document.querySelector('div.popup');
+let btnClickMe = document.querySelector('.btn-main');
+
+btnClickMe.addEventListener('click', showPopup);
+
+function showPopup() {
+    divPopup.classList.add('active');
+}
