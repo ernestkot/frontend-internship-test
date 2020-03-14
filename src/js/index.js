c2436a4e3aab654298fcd63fa11c4ceb11e1d4c5
Ernest <ernest.kot98gmail.com>
2020-03-14 02:32:15 +0100

    Task 3

--- src/js/index.js
+++ src/js/index.js
@@ -9,6 +9,7 @@ const inputsInFormPopup = document.querySelectorAll('input');
 btnClickMe.addEventListener('click', showPopup);
 closeButton.addEventListener('click', closePopup);
 
+
 function showPopup() {
     divPopup.classList.add('active');
 }
@@ -32,12 +33,20 @@ const inputCheckbox = document.querySelector('input[type="checkbox"]');
 const emailErrorMessage = document.querySelector('.popup-form-email-error');
 const checkboxErrorMessage = document.querySelector('.popup-form-checkbox-error');
 const passwordErrorMessage = document.querySelector('.popup-form-password-error');
+const successMessage = document.querySelector('.success-message');
+const btnSubmit = document.querySelector('.btn-submit');
+
 
 formPopup.setAttribute("novalidate", true);
-formPopup.addEventListener('submit', function (event) {
-    event.preventDefault();
 
-    let formPopupErrors = [];
+closeButton.addEventListener('click', removeErrorMessages);
+btnSubmit.addEventListener('click', function (event) {
+    event.preventDefault();
+    checkPopupFormValidity();
+});
+
+
+function removeErrorMessages() {
 
     if (inputEmail.checkValidity()) {
         emailErrorMessage.classList.remove('active');
@@ -50,6 +59,12 @@ formPopup.addEventListener('submit', function (event) {
     if (inputCheckbox.checkValidity()) {
         checkboxErrorMessage.classList.remove('active');
     }
+}
+
+function checkPopupFormValidity() {
+    let formPopupErrors = [];
+
+    removeErrorMessages();
 
     if (!inputEmail.checkValidity()) {
         formPopupErrors.push('');
@@ -66,8 +81,14 @@ formPopup.addEventListener('submit', function (event) {
     }
 
     if (formPopupErrors.length == 0) {
-        event.target.submit();
+        setTimeout(showSuccessMessage, 3000);
     } else {
         alert("Something went wrong");
     }
-});
+}
+
+function showSuccessMessage() {
+    btnClickMe.classList.add('inactive');
+    closePopup();
+    successMessage.classList.add('active');
+}
